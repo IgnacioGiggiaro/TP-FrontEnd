19e3e39b65f41b606c577b4f725ce417ebb2ab2a
TomboliniSantiago <santitombo@gmail.com>
2022-09-12 17:41:02 -0300

    list one prof partial commit

--- src/app/components/Professional/list-one-prof/list-one-prof.component.ts
+++ src/app/components/Professional/list-one-prof/list-one-prof.component.ts
@@ -3,6 +3,7 @@ import { ToastrService } from 'ngx-toastr';
 import {Professional} from "../../../models/professional";
 import {ProfessionalService} from "../../../services/professional.service";
 import {ActivatedRoute, Router} from "@angular/router";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 @Component({
   selector: 'app-list-one-prof',
   templateUrl: './list-one-prof.component.html',
@@ -10,26 +11,39 @@ import {ActivatedRoute, Router} from "@angular/router";
 })
 export class ListOneProfComponent implements OnInit {
 
-  _id: string | null;
+  profForm: FormGroup;
+  id: string | null;
+  constructor(private fb: FormBuilder,
+              private router: Router,
+              private toastr: ToastrService,
+              private _profService: ProfessionalService,
+              private aRouter: ActivatedRoute) {
+    this.profForm = this.fb.group({
+
+      nombre: ['', Validators.required],
+
+    })
+    this.id = this.aRouter.snapshot.paramMap.get('id');}
+
+  ngOnInit(): void { this.getProfessionalByID(this.id)
+  }
+
+  getProfessionalByID(_id: any) {
+    if (_id !== null) {
+      this._profService.getProfessional(_id).subscribe(
+        res => {
+          console.log(res)
+          this.profForm.setValue({
+            nombre: res.nombre+res.apellido
+          })
+        }, error => {
+          console.log(error)
+        }
+      )
+    }
+  }
 
 
-  constructor(private _professionalService: ProfessionalService,
-              private aRouter: ActivatedRoute,
-              public _prof: Professional) {
-    this._id = this.aRouter.snapshot.paramMap.get('_id');
-  }
-  ngOnInit(): void {
-    this.getProfessionalByID(this._id)
-  }
-
-  getProfessionalByID(_id: any){
-    this._professionalService.getProfessional(_id).subscribe(
-      res=>{
-        console.log(res),
-        this._prof=(res)
-      }, error => {
-        console.log(error)
-      }
-    )
-  }
 }
+
+
